2fcec77327f0560d547333dbf44b972e6434fdca
Naomi <115239942+geonaomi@users.noreply.github.com>
2024-07-25 13:51:08 +0000

    Init Docs

---
+++ docs/api.md
@@ -0,0 +1,2 @@
+## API REF
+::: pythag.source
---
+++ src/pythag/source.py
@@ -0,0 +1,9 @@
+def hypot(a, b):
+    """hypot
+
+    Args:
+        a (int, float): _description_
+        b (int, float): _description_
+    """
+    return (a**2 + b**2)**0.5
+
